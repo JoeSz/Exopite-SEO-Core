c69ce9f03e6e5a30d635d2b2ac0ae8ca03698c74
Joe <ujbeszel@gmail.com>
2018-03-21 17:00:30 +0100

    Add JavaScript for Cookie Note/Permission

--- exopite-seo-core/public/js/exopite-seo-core-public.js
+++ exopite-seo-core/public/js/exopite-seo-core-public.js
@@ -1,32 +1,7 @@
-(function( $ ) {
-	'use strict';
-
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
-
-})( jQuery );
+(function($){'use strict';$(document).ready(function(){if($('.cookie-container').length){function createCookie(name,value,days){if(days){var date=new Date();date.setTime(date.getTime()+(days*24*60*60*1000));var expires="; expires="+date.toGMTString()}
+else var expires="";document.cookie=name+"="+value+expires+"; path=/"}
+function readCookie(name){var nameEQ=name+"=";var ca=document.cookie.split(';');for(var i=0;i<ca.length;i++){var c=ca[i];while(c.charAt(0)==' ')c=c.substring(1,c.length);if(c.indexOf(nameEQ)==0)return c.substring(nameEQ.length,c.length)}
+return null}
+function eraseCookie(name){createCookie(name,"",-1)}
+var cookie=readCookie('accept-cookies');if(readCookie('accept-cookies')===null){$('.cookie-container').show(300)}
+$('.accept-cookies-js').on('click',function(event){event.preventDefault();createCookie('accept-cookies','accepted',30);$('.cookie-container').hide(300)})}})})(jQuery)
