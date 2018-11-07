042d2550ab5be81ef0570082363ba3f4d89d703f
Joe <ujbeszel@gmail.com>
2018-11-07 11:17:47 +0100

    Optimize minified JavaScript

--- exopite-seo-core/public/js/exopite-seo-core-public.js
+++ exopite-seo-core/public/js/exopite-seo-core-public.js
@@ -1,7 +1,1 @@
-(function($){'use strict';$(document).ready(function(){if($('.cookie-container').length){function createCookie(name,value,days){if(days){var date=new Date();date.setTime(date.getTime()+(days*24*60*60*1000));var expires="; expires="+date.toGMTString()}
-else var expires="";document.cookie=name+"="+value+expires+"; path=/"}
-function readCookie(name){var nameEQ=name+"=";var ca=document.cookie.split(';');for(var i=0;i<ca.length;i++){var c=ca[i];while(c.charAt(0)==' ')c=c.substring(1,c.length);if(c.indexOf(nameEQ)==0)return c.substring(nameEQ.length,c.length)}
-return null}
-function eraseCookie(name){createCookie(name,"",-1)}
-var cookie=readCookie('accept-cookies');if(readCookie('accept-cookies')===null){$('.cookie-container').show(300)}
-$('.accept-cookies-js').on('click',function(event){event.preventDefault();createCookie('accept-cookies','accepted',30);$('.cookie-container').hide(300)})}})})(jQuery)
+(function(a){'use strict';a(document).ready(function(){if(a('.cookie-container').length){function d(g,h,j){if(j){var k=new Date;k.setTime(k.getTime()+1e3*(60*(60*(24*j))));var l='; expires='+k.toGMTString()}else var l='';document.cookie=g+'='+h+l+'; path=/'}function e(g){for(var l,h=g+'=',j=document.cookie.split(';'),k=0;k<j.length;k++){for(l=j[k];' '==l.charAt(0);)l=l.substring(1,l.length);if(0==l.indexOf(h))return l.substring(h.length,l.length)}return null}e('accept-cookies');null===e('accept-cookies')&&a('.cookie-container').show(300),a('.accept-cookies-js').on('click',function(g){g.preventDefault(),d('accept-cookies','accepted',30),a('.cookie-container').hide(300)})}})})(jQuery);
