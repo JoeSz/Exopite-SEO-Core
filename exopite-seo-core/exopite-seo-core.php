f83ebc9627526691708bb479f99bac266db22948
Joe <ujbeszel@gmail.com>
2019-03-03 19:59:47 +0100

    Update update link

--- exopite-seo-core/exopite-seo-core.php
+++ exopite-seo-core/exopite-seo-core.php
@@ -83,7 +83,7 @@ if ( is_admin() ) {
     }
 
     $MyUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-        'https://update.szalai.org/?action=get_metadata&slug=' . EXOPITE_SEO_PLUGIN_NAME, //Metadata URL.
+        'https://update.joeszalai.org/?action=get_metadata&slug=' . EXOPITE_SEO_PLUGIN_NAME, //Metadata URL.
         __FILE__, //Full path to the main plugin file.
         EXOPITE_SEO_PLUGIN_NAME //Plugin slug. Usually it's the same as the name of the directory.
     );
