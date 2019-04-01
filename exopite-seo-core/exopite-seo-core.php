7472230debe0d4ae1bf96c54cb1ef3b2b07f42d1
Joe <ujbeszel@gmail.com>
2019-04-01 06:23:54 +0200

    Update version

--- exopite-seo-core/exopite-seo-core.php
+++ exopite-seo-core/exopite-seo-core.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'EXOPITE_SEO_CORE_VERSION', '20181123' );
+define( 'EXOPITE_SEO_CORE_VERSION', '20190401' );
 define( 'EXOPITE_SEO_PLUGIN_NAME', 'exopite-seo-core' );
 define( 'EXOPITE_SEO_PATH', plugin_dir_path( __FILE__ ) );
 
