2a07c63dc2720a7b728091d895acdedf1f6a9491
Joe <ujbeszel@gmail.com>
2021-04-07 13:59:33 +0200

    Update version

--- exopite-seo-core/exopite-seo-core.php
+++ exopite-seo-core/exopite-seo-core.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Exopite SEO Core
  * Plugin URI:        https://joe.szalai.org
  * Description:       Core SEO optimizations with cookie notice, inline CSS and more.
- * Version:           20210324
+ * Version:           20210407
  * Author:            Joe Szalai
  * Author URI:        https://joe.szalai.org
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'EXOPITE_SEO_CORE_VERSION', '20210324' );
+define( 'EXOPITE_SEO_CORE_VERSION', '20210407' );
 define( 'EXOPITE_SEO_PLUGIN_NAME', 'exopite-seo-core' );
 define( 'EXOPITE_SEO_PATH', plugin_dir_path( __FILE__ ) );
 
